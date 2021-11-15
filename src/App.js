803a21414c5b2a57fdd6a4b622b5ace64b2f6dfd
GustavoSierra <gsierra.business@gmail.com>
2021-11-15 01:42:06 +0100

    added Tasks and Task to components, with first button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import { useState } from 'react'
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Cita con el doctor',
+        day: '16 de Febrero a las 2:30pm',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Reunion en Geekshubs',
+        day: '17 de Febrero a las 1:30pm',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Hacer la compra',
+        day: '18 de Febrero a las 5:30pm',
+        reminder: false
+    }
+]);
 
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,15 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
 const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('click');
+    }
+    
     return (
-        <header>
-            <h1 style={{ color: 'red', backgroundColor: 'black' }}>{title}</h1>
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color='blue' text='Añadir' onClick={onClick}/>
         </header>
     )
 }
@@ -10,4 +18,9 @@ Header.defaultProps = {
     title: 'Rastreador de Tareas React'
 }
 
+// CSS in JavaScript
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black'
+// }
 export default Header
